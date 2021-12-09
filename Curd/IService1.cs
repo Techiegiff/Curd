e9cffbe2db497dae61fcd373cf379a18af59c739
Maria Gifty <giftyjy1999@gmail.com>
2021-12-09 16:56:31 +0530

    Add project files.

---
+++ Curd/IService1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Curd
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        InsertUser GetDataUsingDataContract(InsertUser user);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class InsertUser
+    {
+        string Bookname = string.Empty;
+        int Bookid = int.Parse(string.Empty);
+        string Bookauthor = string.Empty;
+        float Bookprice = float.Parse(string.Empty);
+
+        [DataMember]
+        public int BookID
+        {
+            get { return Bookid; }
+            set { Bookid = value; }
+        }
+
+        [DataMember]
+        public string BookName
+        {
+            get { return Bookname; }
+            set { Bookname = value; }
+        }
+        [DataMember]
+        public string BookAuthor
+        {
+            get { return Bookauthor; }
+            set { Bookauthor = value; }
+        }
+        [DataMember]
+        public float BookPrice
+        {
+            get { return Bookprice; }
+            set { Bookprice = value; }
+        }
+    }
+}
---
+++ Curd/Service1.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Curd
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public InsertUser Insert(InsertUser user)
+        {
+            SqlConnection sqlcon = new SqlConnection("server=DIOEAMORA;database=ustglobaldb;Integrated Security=true");
+            sqlcon.Open();
+            SqlCommand smd = new SqlCommand("insert into BOOK(BookId,BookName,BookAuthor,BookPrice) values(@bid,@bnm,@bauth,@bp)", sqlcon);
+            smd.Parameters.AddWithValue("@bid", user.BookID);
+            smd.Parameters.AddWithValue("@bnm", user.BookName);
+            smd.Parameters.AddWithValue("@bauth",user.BookAuthor);
+            smd.Parameters.AddWithValue("@bp", user.BookPrice);
+            smd.ExecuteNonQuery();
+            return user;
+        }
+
+        public string GetData(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        InsertUser IService1.GetDataUsingDataContract(InsertUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
